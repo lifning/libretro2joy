fa2d61e0e1898cb4d339ae034afb5d3e7c7c7616
lif <lifning@localhost>
2012-10-13 20:03:26 -0400

    Initial analog support.  RumblePad2-style remapping. May switch to Xbox-style if it means a less ugly Windows port down the road. For that matter, may make them both options.

--- libretro.c
+++ libretro.c
@@ -57,86 +57,130 @@ void retro_reset(void)
 {
 }
 
-static int uinp_fd[8] = {-1, -1, -1, -1, -1, -1, -1, -1};
+#define NUM_PADS 8
+int uinp_fd[NUM_PADS] = {-1, -1, -1, -1, -1, -1, -1, -1};
+inline void close_uinp_fd(unsigned port) {
+	close(uinp_fd[port]);
+	uinp_fd[port] = -1;
+}
+#define ioctl_warn(fd, val, arg) { \
+	if ( ioctl(fd, val, arg) ) \
+		perror("ioctl " #val " " #arg); \
+}
+inline void ioctl_set_absbit(int fd, struct uinput_user_dev *p_uinp,
+                             int axis, int low, int high) {
+	ioctl_warn(fd, UI_SET_ABSBIT, axis);
+	p_uinp->absmin[axis] = low;
+	p_uinp->absmax[axis] = high;
+}
 void retro_set_controller_port_device(unsigned port, unsigned device)
 {
 	int i;
-	struct uinput_user_dev uinp;
 
 	printf("set_controller_port_device(%d, %d)\n", port, device);
 	if ( device == RETRO_DEVICE_NONE ) {
 		// unplug, if it's already plugged.
 		if ( uinp_fd[port] != -1 ) {
-			if ( ioctl(uinp_fd[port], UI_DEV_DESTROY) )
-				perror("ioctl UI_DEV_DESTROY");
-			close(uinp_fd[port]);
-			uinp_fd[port] = -1;
+			ioctl_warn(uinp_fd[port], UI_DEV_DESTROY, 0);
+			close_uinp_fd(port);
 		}
-		return;
-	}
-
-	if ( device != RETRO_DEVICE_JOYPAD ) {
+	} else if ( device != RETRO_DEVICE_JOYPAD ) {
 		puts("device types other than JOYPAD not yet implemented.");
-		return;
-	}
-
-	if ( uinp_fd[port] != -1 ) {
+	} else if ( uinp_fd[port] != -1 ) {
 		printf("port %d already has controller (fd %d).\n", port, uinp_fd[port]);
-		return;
-	}
-
-	uinp_fd[port] = open( "/dev/uinput", O_WRONLY | O_NONBLOCK );
-	if ( uinp_fd[port] == -1 ) {
+	} else if ( (uinp_fd[port] = open("/dev/uinput", O_WRONLY | O_NONBLOCK)) == -1 ) {
 		perror("opening /dev/uinput");
-		return;
-	}
+	} else {
+		/* Define our virtual input device. */
+		struct uinput_user_dev uinp;
+		memset(&uinp, 0, sizeof(uinp));
+		snprintf(uinp.name, UINPUT_MAX_NAME_SIZE, "%s[%d]", _info.library_name, port);
+		uinp.id.bustype = BUS_USB;
+		uinp.id.vendor = 0xf00d;
+		uinp.id.product = 0xbeef;
+		uinp.id.version = 1;
+		uinp.ff_effects_max = 0; // otherwise Bad Things happen in UI_DEV_CREATE
+		// it's technically covered by the memset, but worth explicitly stating.
 
-	/* Keycodes in the BTN_JOYSTICK range make this a joystick device. */
-	if ( ioctl(uinp_fd[port], UI_SET_EVBIT, EV_KEY) )
-		perror("ioctl UI_SET_EVBIT");
-	for( i = 0; i < 16; i ++ )
-		if ( ioctl(uinp_fd[port], UI_SET_KEYBIT, BTN_JOYSTICK + i) )
-			perror("ioctl UI_SET_KEYBIT");
+		/* Keycodes in the BTN_JOYSTICK range make this a joystick device. */
+		ioctl_warn(uinp_fd[port], UI_SET_EVBIT, EV_KEY);
+		for( i = 0; i < 12; ++i )
+			ioctl_warn(uinp_fd[port], UI_SET_KEYBIT, BTN_JOYSTICK+i);
+		/* analogue sticks and POV hat */
+		ioctl_warn(uinp_fd[port], UI_SET_EVBIT, EV_ABS);
+		ioctl_set_absbit(uinp_fd[port], &uinp, ABS_X, -32767, 32767);
+		ioctl_set_absbit(uinp_fd[port], &uinp, ABS_Y, -32767, 32767);
+		ioctl_set_absbit(uinp_fd[port], &uinp, ABS_Z, -32767, 32767);
+		ioctl_set_absbit(uinp_fd[port], &uinp, ABS_RZ, -32767, 32767);
+		ioctl_set_absbit(uinp_fd[port], &uinp, ABS_HAT0X, -1, 1);
+		ioctl_set_absbit(uinp_fd[port], &uinp, ABS_HAT0Y, -1, 1);
+		/* be able to sync when done sending events */
+		ioctl_warn(uinp_fd[port], UI_SET_EVBIT, EV_SYN);
 
-	/* Define our virtual input device. */
-	snprintf(uinp.name, UINPUT_MAX_NAME_SIZE, "%s[%d]", _info.library_name, port);
-	uinp.id.bustype = BUS_USB;
-	uinp.id.vendor = 0xf00d;
-	uinp.id.product = 0xbeef;
-	uinp.id.version = 1;
-	uinp.ff_effects_max = 0; // otherwise Bad Things happen in UI_DEV_CREATE.
-
-	if ( write( uinp_fd[port], &uinp, sizeof(uinp) ) != sizeof(uinp) ) {
-		perror("writing uinput_user_dev");
-		close(uinp_fd[port]);
-		uinp_fd[port] = -1;
-		return;
-	}
-	if ( ioctl(uinp_fd[port], UI_DEV_CREATE) ) {
-		perror("ioctl UI_DEV_CREATE");
-		close(uinp_fd[port]);
-		uinp_fd[port] = -1;
-		return;
+		/* Write device definition and create. */
+		if ( write(uinp_fd[port], &uinp, sizeof(uinp)) != sizeof(uinp) ) {
+			perror("writing uinput_user_dev");
+			close_uinp_fd(port);
+		} else if ( ioctl(uinp_fd[port], UI_DEV_CREATE) ) {
+			perror("ioctl UI_DEV_CREATE");
+			close_uinp_fd(port);
+		}
 	}
 }
 
+// Mimics the Logitech RumblePad 2 USB mappings.
+const static int BUTTON_MAP[12] = {
+	RETRO_DEVICE_ID_JOYPAD_Y,
+	RETRO_DEVICE_ID_JOYPAD_B,
+	RETRO_DEVICE_ID_JOYPAD_A,
+	RETRO_DEVICE_ID_JOYPAD_X,
+	RETRO_DEVICE_ID_JOYPAD_L,
+	RETRO_DEVICE_ID_JOYPAD_R,
+	RETRO_DEVICE_ID_JOYPAD_L2,
+	RETRO_DEVICE_ID_JOYPAD_R2,
+	RETRO_DEVICE_ID_JOYPAD_SELECT,
+	RETRO_DEVICE_ID_JOYPAD_START,
+	RETRO_DEVICE_ID_JOYPAD_L3,
+	RETRO_DEVICE_ID_JOYPAD_R3,
+};
+const static int AXIS_MAP[4][3] = {
+	{ ABS_X,  RETRO_DEVICE_INDEX_ANALOG_LEFT,  RETRO_DEVICE_ID_ANALOG_X },
+	{ ABS_Y,  RETRO_DEVICE_INDEX_ANALOG_LEFT,  RETRO_DEVICE_ID_ANALOG_Y },
+	{ ABS_Z,  RETRO_DEVICE_INDEX_ANALOG_RIGHT, RETRO_DEVICE_ID_ANALOG_X },
+	{ ABS_RZ, RETRO_DEVICE_INDEX_ANALOG_RIGHT, RETRO_DEVICE_ID_ANALOG_Y },
+};
+inline void send_event(int fd, int type, int code, int value) {
+	struct input_event ev = { .type = type, .code = code, .value = value };
+	if ( write(fd, &ev, sizeof(ev)) != sizeof(ev) ) {
+		perror("writing input_event");
+	}
+}
 void retro_run(void)
 {
-	int i, port;
+	int i, fd, port, value;
 	input_poll_cb();
-	for ( port = 0; port < 8; ++port ) {
-		if ( uinp_fd[port] == -1 ) { continue; }
-		for ( i = 0; i < 16; ++i ) {
-			struct input_event ev = {
-				.type = EV_KEY,
-				.code = BTN_JOYSTICK + i,
-				.value = input_state_cb(port, RETRO_DEVICE_JOYPAD, 0, i),
-			};
-			if ( write(uinp_fd[port], &ev, sizeof(ev)) != sizeof(ev) ) {
-				perror( "write" );
+	for ( port = 0; port < NUM_PADS; ++port ) {
+		if ( uinp_fd[port] != -1 ) {
+			fd = uinp_fd[port];
+			// buttons
+			for ( i = 0; i < sizeof(BUTTON_MAP)/sizeof(BUTTON_MAP[0]); ++i ) {
+				value = input_state_cb(port, RETRO_DEVICE_JOYPAD, 0, BUTTON_MAP[i]);
+				send_event(fd, EV_KEY, BTN_JOYSTICK + i, value);
 			}
+			// axes
+			for ( i = 0; i < sizeof(AXIS_MAP)/sizeof(AXIS_MAP[0]); ++i ) {
+				value = input_state_cb(port, RETRO_DEVICE_ANALOG, AXIS_MAP[i][1], AXIS_MAP[i][2]);
+				send_event(fd, EV_ABS, AXIS_MAP[i][0], value);
+			}
+			// 'hat' axes
+			value = !! input_state_cb(port, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_RIGHT);
+			value -= !! input_state_cb(port, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_LEFT);
+			send_event(fd, EV_ABS, ABS_HAT0X, value);
+			value = !! input_state_cb(port, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_DOWN);
+			value -= !! input_state_cb(port, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_UP);
+			send_event(fd, EV_ABS, ABS_HAT0Y, value);
+			send_event(fd, EV_SYN, SYN_REPORT, 0);
 		}
-		//input_state_cb(0, RETRO_DEVICE_ANALOG, RETRO_DEVICE_INDEX_ANALOG_LEFT, RETRO_DEVICE_ID_ANALOG_X);
 	}
 }
 
